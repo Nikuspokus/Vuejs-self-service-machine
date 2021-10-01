ef0be2e9802cde794e9168cd3ec059835fb2885b
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-10-01 14:17:59 +0200

    Section 3 - Projet 3 - FINI

--- scripts.js
+++ scripts.js
@@ -24,10 +24,14 @@ const todosApp = {
         },
         clearTodoList: function() {
             this.todos = []
-        }
+        },
     },
     created() {
         this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos
+    },
+    //Permet de mettre à jour la donnée dans le local storage
+    updated() {
+        localStorage.setItem("todos", JSON.stringify(this.todos))
     }
 };
 
