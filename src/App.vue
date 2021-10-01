1c66547bbc159de862e091a6c95a1a9b5347d475
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-10-01 16:16:52 +0200

    Projet 4 - creation api des questions ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Microphones can be used not only to pick up sound, but also to project
+      sound similar to a speaker ?
+    </h1>
+
+    <input type="radio" name="options" value="true" />
+    <label>True</label><br />
+
+    <input type="radio" name="options" value="true" />
+    <label>False</label><br />
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
@@ -21,6 +28,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
