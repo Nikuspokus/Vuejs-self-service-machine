4e66b2f8db9f74e8e82ad81fccdbf3dfed939e28
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-10-03 08:44:53 +0200

    Projet 4 - Soumettre la réponse

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <h1 v-html="this.question">
-    </h1>
-
-    <input type="radio" name="options" value="true" />
-    <label>True</label><br />
-
-    <input type="radio" name="options" value="true" />
-    <label>False</label><br />
-
-    <button class="send" type="button">Send</button>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input
+          type="radio"
+          name="options"
+          :value="answer"
+          v-model="chosen_answer"
+        />
+        <label v-html="answer"></label><br />
+      </template>
+      <button class="send" type="button" @click="this.submitAnswer">
+        Send
+      </button>
+    </template>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
   data() {
@@ -22,28 +26,45 @@ export default {
       question: undefined,
       incorrectAnswers: undefined,
       correctAnswers: undefined,
-    }
-  }, 
+      chosen_answer: undefined,
+    };
+  },
   computed: {
     answers() {
-      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers))
-      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer)
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswers
+      );
       return answers;
-    }
-  }, 
-  created(){
+    },
+  },
+
+  methods: {
+    submitAnswer() {
+      if (!this.chosen_answer) {
+        alert('Pick one of the options')
+      } else {
+        if (this.chosen_answer == this.correctAnswers) {
+          alert('You got it right !')
+        } else {
+          alert('You got it wrong !')
+        }
+      }
+    },
+  },
+
+  created() {
     this.axios
-    .get('https://opentdb.com/api.php?amount=1&category=20&difficulty=easy')
-    .then((response) => {
-      this.question = response.data.results[0].question;
-      this.incorrectAnswers = response.data.results[0].incorrect_answers;
-      this.correctAnswers = response.data.results[0].correct_answer;
-  })
-  }
-  
-}
-
-
+      .get("https://opentdb.com/api.php?amount=1&category=20&difficulty=easy")
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswers = response.data.results[0].correct_answer;
+      });
+  },
+};
 </script>
 
 <style lang="scss">
